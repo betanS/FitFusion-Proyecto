e5c8831844b5bb27a17813e1f39092cdd83bca3f
Alejandro <77583757+betanS@users.noreply.github.com>
2024-03-15 14:26:24 +0000

    Update Proyect
    modifiqué el main y agregué pantallas de login básicas

--- app/src/main/java/com/example/fitfusion/MainActivity.kt
+++ app/src/main/java/com/example/fitfusion/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +18,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.fitfusion.ui.theme.FitFusionTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,8 +36,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    App()
                 }
+            }
+        }
+    }
+}
+@Composable
+fun App(
+    navController: NavHostController = rememberNavController()
+) {
+
+    Scaffold() { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = "home"
+        ) {
+            composable(route = "home") {
+                StartScreen(navController)
+            }
+            composable(
+                route = "login",
+            ) {
+                LoginScreen(navController)
+            }
+            composable(
+                route = "register"
+            ) {
+                RegisterScreen(navController)
+            }
+            composable(
+                route = "inicio"
+            ) {
+                InicioScreen(navController)
             }
         }
     }
